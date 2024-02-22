752ef194795c39fe84d06af612588b553273472a
yaguilar-2022014 <yaguilar-2022014@kinal.edu.gt>
2024-02-22 13:36:21 -0600

    Se agregó el proyecto RandomUsers

---
+++ RANDOMUSERSAV/user.js
@@ -0,0 +1,25 @@
+//Asíncrona
+//Esperar a que se ejecute por completo una instrcuciión, sin obstreir
+// el hilo de procesoss. 
+
+//Formas de manejar la asincronía:
+
+/**
+ * 1. Callbacks -> Desuso!!!
+ * 2. Promesas
+ * 3. Async / Await -> La mejor opción
+ * 
+*/
+
+//CALLBACKS
+function getUsersWhithCallback(callback){
+
+}
+//PROMISES
+const getUsersWhithPromise = ()=>{
+
+}
+//ASYNC / AWAIT
+const getUsersWhithAsync = async ()=>{
+    
+}
